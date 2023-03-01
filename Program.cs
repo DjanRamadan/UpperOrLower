c19086f6bf2906a7d6d8645327eae521b4da259a
Djan <124775935+DjanRamadan@users.noreply.github.com>
2023-03-01 13:25:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter a character: ");
+char w1 = Console.ReadLine()[0];
+
+if (w1 >=65 && w1 <=90)
+{
+    Console.WriteLine("Your character is upper-case.");
+}
+else
+{
+    Console.WriteLine("Your character is lower-case.");
+}
